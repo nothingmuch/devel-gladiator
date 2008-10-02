7a48f9de3305c580fe5ebaf722b05468dce25990
Yuval Kogman <nothingmuch@woobling.org>
2008-10-02 09:40:11 -0400

    add docs

--- lib/Devel/Gladiator.pm
+++ lib/Devel/Gladiator.pm
@@ -55,3 +55,61 @@ XSLoader::load('Devel::Gladiator', $XS_VERSION);
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+Devel::Gladiator - Walk Perl's arena
+
+=head1 SYNOPSIS
+
+	use Devel::Gladiator qw(walk_arena arena_ref_counts arena_table);
+
+    my $all = walk_arena();
+
+	foreach my $sv ( @$all ) {
+		warn "live object: $sv\n";
+	}
+
+	warn arena_table(); # prints counts keyed by class
+
+=head1 DESCRIPTION
+
+L<Devel::Gladiator> iterate's Perl's internal memory structures and can be used
+to enumerate all the currently live SVs.
+
+This can be used to hunt leaks and to profile memory usage.
+
+=head1 EXPORTS
+
+=over 4
+
+=item walk_arena
+
+Returns an array reference containing all the live SVs.
+
+=item arena_ref_counts
+
+Returns a hash keyed by class and reftype of all the live SVs.
+
+This is a convenient way to find out how many objects of a given class exist at
+a certain point.
+
+=item arena_table
+
+Formats a string table based on C<arena_ref_counts> suitable for printing.
+
+=back
+
+=head1 COPYRIGHT AND LICENCE
+
+Put the correct copyright and licence information here.
+
+Copyright (C) 2006 by Artur Bergman
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.6 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cute
